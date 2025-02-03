6efee747a61af0fadd6fd15491942e52d9d53730
Mark Elek <dev.markelek@gmail.com>
2025-02-03 14:07:51 +0100

    Update: copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'ColorRampConverter'
-copyright = '2022-2024, Mark Elek, David Elek'
+copyright = '2022-2025, Mark Elek, David Elek'
 author = 'Mark Elek, David Elek'
 
 # The full version, including alpha/beta/rc tags
